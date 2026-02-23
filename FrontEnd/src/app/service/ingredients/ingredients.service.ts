482ee4c58a0c439b18d319fb6fc72d9ba0fbaa86
omarcherif2866 <101433661+omarcherif2866@users.noreply.github.com>
2026-02-23 15:06:07 +0100

    Update ingredients.service.ts

--- FrontEnd/src/app/service/ingredients/ingredients.service.ts
+++ FrontEnd/src/app/service/ingredients/ingredients.service.ts
@@ -1,31 +1,62 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { Ingredient } from 'src/app/Models/ingredients/ingredient';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class IngredientsService {
+
+//   constructor(private _http: HttpClient) {}
+
+//   addIngredient(data: any): Observable<Ingredient> {
+//     return this._http.post<Ingredient>('http://localhost:9090/ingredients/AddIngredients',data)
+//   }
+
+//   updateIngredient(id: number, data: any){
+//     return this._http.put<Ingredient>(`http://localhost:9090/ingredients/${id}`, data);
+//   }
+
+//   getIngredientList(){
+//     return this._http.get<Ingredient[]>('http://localhost:9090/ingredients/');
+//   }
+
+//   deleteIngredient(id: number) {
+//     return this._http.delete<Ingredient>(`http://localhost:9090/ingredients/${id}`);
+//   }
+
+
+
+// }
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Ingredient } from 'src/app/Models/ingredients/ingredient';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientsService {
+  
+  private apiUrl = environment.apiUrl;
 
   constructor(private _http: HttpClient) {}
 
   addIngredient(data: any): Observable<Ingredient> {
-    return this._http.post<Ingredient>('http://localhost:9090/ingredients/AddIngredients',data)
+    return this._http.post<Ingredient>(`${this.apiUrl}/ingredients/AddIngredients`, data);
   }
 
-  updateIngredient(id: number, data: any){
-    return this._http.put<Ingredient>(`http://localhost:9090/ingredients/${id}`, data);
+  updateIngredient(id: number, data: any) {
+    return this._http.put<Ingredient>(`${this.apiUrl}/ingredients/${id}`, data);
   }
 
-  getIngredientList(){
-    return this._http.get<Ingredient[]>('http://localhost:9090/ingredients/');
+  getIngredientList() {
+    return this._http.get<Ingredient[]>(`${this.apiUrl}/ingredients/`);
   }
 
   deleteIngredient(id: number) {
-    return this._http.delete<Ingredient>(`http://localhost:9090/ingredients/${id}`);
+    return this._http.delete<Ingredient>(`${this.apiUrl}/ingredients/${id}`);
   }
-
-
-
 }
