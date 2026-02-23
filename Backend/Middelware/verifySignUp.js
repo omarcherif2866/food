dad4507fa3e0fa34b137598c252415a548abfbf3
omarcherif2866 <101433661+omarcherif2866@users.noreply.github.com>
2026-02-23 13:35:50 +0100

    Update verifySignUp.js

--- Backend/Middelware/verifySignUp.js
+++ Backend/Middelware/verifySignUp.js
@@ -1,5 +1,5 @@
-import  User  from "../Models/user.js";
-import Role from "../Models/role.js";
+import  User  from "../models/user.js";
+import Role from "../models/role.js";
 
 const checkDuplicateUsernameOrEmail = async (req, res, next) => {
   try {
