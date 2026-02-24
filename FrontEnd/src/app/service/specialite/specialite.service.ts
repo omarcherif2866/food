d6d02a9a774c911c085f85034ea96ac264d76e3c
omarcherif2866 <101433661+omarcherif2866@users.noreply.github.com>
2026-02-24 10:02:34 +0100

    Update specialite.service.ts

--- FrontEnd/src/app/service/specialite/specialite.service.ts
+++ FrontEnd/src/app/service/specialite/specialite.service.ts
@@ -2,27 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Specialite } from 'src/app/Models/specialite/specialite';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SpecialiteService {
-
+  private apiUrl = environment.apiUrl;
   constructor(private _http: HttpClient) {}
 
   addSpecialite(data: any): Observable<Specialite> {
-    return this._http.post<Specialite>('http://localhost:9090/specialites/AddSpecialite',data)
+    return this._http.post<Specialite>(`${this.apiUrl}/specialites/AddSpecialite`,data)
   }
 
   updateSpecialite(id: number, data: any){
-    return this._http.put<Specialite>(`http://localhost:9090/specialites/${id}`, data);
+    return this._http.put<Specialite>(`${this.apiUrl}/specialites/${id}`, data);
   }
 
   getSpecialiteList(){
-    return this._http.get<Specialite[]>('http://localhost:9090/specialites/');
+    return this._http.get<Specialite[]>(`${this.apiUrl}/specialites/`);
   }
 
   deleteSpecialite(id: number) {
-    return this._http.delete<Specialite>(`http://localhost:9090/specialites/${id}`);
+    return this._http.delete<Specialite>(`${this.apiUrl}/specialites/${id}`);
   }
 
 
